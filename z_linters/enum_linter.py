4c13ec395323cdc75f65e05de22723e39452a74d
ScatteredComet <benjaminleogreen@gmail.com>
2026-03-06 05:11:49 +0000

    visuals: make printed linting data even prettier :)

--- z_linters/enum_linter.py
+++ z_linters/enum_linter.py
@@ -26,7 +26,7 @@ xml_content = ""
 
 g = list(doc_path.rglob("*.xml"))
 
-for p in tqdm(g, desc="caching all xml files", total=len(g)):
+for p in tqdm(g, desc="Caching all *.xml files...", total=len(g)):
     with open(p, 'r', encoding='utf-8') as f:
         xml_content += f.read()
 
@@ -61,10 +61,19 @@ def simple_find_in_xml(phrase : str, debug_mode : bool) -> bool:
 
     return False
 
+visibility_keywords = re.compile(r'(?P<scope>\b(?:public|private|protected)\s*:)')
+
+EMPTY_HEADER : str = f"{'///':<14} {LIGHT_BLUE}{'///':<60} {'///':<60} {'///':<60}{RESET} {'///':<10} {'///'}"
+
+HEADER : str = f"{'violation no.':<14} {LIGHT_BLUE}{'enum name':<60} {'violation type':<60} {'constant name':<60}{RESET} {'scope':<10} {'filepath'}"
+
+MINIMUM_NUMBER_OF_CAPITALS : int = 2
 
 def main():
     
     violation_number : int = 0
+    number_of_missing_constants : int = 0
+    number_of_missing_types : int = 0
     
     # paths = [Path("core/"), Path("modules/"), Path("scene/"), Path("servers/")]
     paths = [Path("core/"), Path("modules/"), Path("scene/")]
@@ -75,12 +84,19 @@ def main():
         to_chain.append(list(path.rglob("*.cpp")))
         to_chain.append(list(path.rglob("*.h")))
 
+    # make look fancy
+    tqdm.write(EMPTY_HEADER)
+    tqdm.write(HEADER)
+    tqdm.write(EMPTY_HEADER)
+    tqdm.write("")
+
     for p in tqdm(chain(*to_chain), total=sum(len(x) for x in to_chain)):
 
         enum_started : bool = False
         enum_name_found : bool = False
         debug_mode = False
         enum_name : str = ""
+        current_scope : str = ""
 
         if "modules/gdscript" in str(p) or "extensions" in str(p) or "profiler" in str(p):
             continue
@@ -94,7 +110,7 @@ def main():
                     
                     uppercase_count = sum(1 for char in enum_name if char.isupper())
 
-                    if enum_name_found and uppercase_count > 1: # only check if we know there is documentation for this enum, and the enum name is interesting
+                    if enum_name_found and uppercase_count >= MINIMUM_NUMBER_OF_CAPITALS: # only check if we know there is documentation for this enum, and the enum name is interesting
                         # strip last comma from line and leading whitespace; then try to find that string in a *.xml.
                         stripped = line.strip()
                         if stripped:
@@ -102,16 +118,21 @@ def main():
                             enum_value_name = re.sub(r'[^A-Z0-9_]', '', stripped.split()[0])  
 
                             if not cached_found_in_xml(re.compile(rf"{enum_value_name}")):
-                                type_val = f"Type: value        {enum_value_name}"
-                                if "MAX" in enum_value_name:
+                                type = f"Enumeration constant"
+                                if "MAX" in enum_value_name or "NUM_BITS" in enum_value_name:
+                                    # type = f"Delimiter"
+                                    # tqdm.write(f"{violation_number:<14} {YELLOW}{enum_name:<60} {type:<60} {enum_value_name:<60}{RESET} {current_scope:<10} {p}")
                                     pass
-                                    # tqdm.write(f"{violation_number:<10} {GREEN}Documentation missing for enum:{RESET} {enum_name:<50} {YELLOW}{type_val:<100}{RESET} {p}")
-                                else:
-                                    tqdm.write(f"{violation_number:<10} {GREEN}Documentation missing for enum:{RESET} {enum_name:<50} {type_val:<100} {p}")
+                                elif current_scope == "public":
+                                    tqdm.write(f"{'':<7} {number_of_missing_constants:<7} {GREEN}{enum_name:<60} {type:<60} {enum_value_name:<60}{RESET} {current_scope:<10} {p}")
                                     violation_number += 1
-                                    pass
+                                    number_of_missing_constants += 1
                 
                 match = enum_line.search(line)
+                scope = visibility_keywords.search(line)
+
+                if scope:
+                    current_scope = scope.group('scope').strip(':').strip()
 
                 if match:
                     enum_started = True
@@ -119,12 +140,13 @@ def main():
 
                     if not cached_simple_found_in_xml(rf'enum="{enum_name}"', debug_mode):
                         enum_name_found = False
-                        type_all = "Type: all"
+                        type_all = "Enumeration type"
 
                         uppercase_count = sum(1 for char in enum_name if char.isupper())
-                        if (uppercase_count > 1):
-                            tqdm.write(f"{violation_number:<10} {LIGHT_BLUE}Documentation missing for enum:{RESET} {enum_name:<50} {type_all:<100} {p}")
+                        if (uppercase_count >= MINIMUM_NUMBER_OF_CAPITALS) and current_scope == "public":
+                            tqdm.write(f"{number_of_missing_types:<7} {'':<7} {LIGHT_BLUE}{enum_name:<60} {type_all:<60} {'':<60}{RESET} {current_scope:<10} {p}")
                             violation_number += 1
+                            number_of_missing_types += 1
                     else:
                         enum_name_found = True
 
